23040a95ad980a8423c11ad4c9cc89aa7ad9967b
KhouloudDa <daghsnikhouloud@gmail.com>
2022-10-05 15:40:51 +0100

    Data Binding JSON

--- webapp/controller/DataBinding.controller.js
+++ webapp/controller/DataBinding.controller.js
@@ -9,7 +9,10 @@ sap.ui.define([
 
 		return Controller.extend("databindingsapui5.controller.DataBinding", {
 			onInit: function () {
-
+				/***Create the model and load data from the local JSON file ***/
+				var oModel = new sap.ui.model.json.JSONModel("./model/Products.json");
+				oModel.setDefaultBindingMode("OneWay");
+				this.getView().setModel(oModel, "ProductsModel");
 			}
 		});
 	});
